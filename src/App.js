4e531ec3e3d5feabeba3e226b766bedbd89c66eb
ming-sil <choming4926@gmail.com>
2022-07-06 13:52:14 +0900

    router_1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { HelmetProvider } from "react-helmet-async";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+
 function App() {
-  return <>MVideo</>;
+  return (
+    <HelmetProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+    </HelmetProvider>
+  );
 }
 
 export default App;
