056de285303c9a5c3851c59a58ba2fe81683776a
ming-sil <choming4926@gmail.com>
2022-07-11 19:09:49 +0900

    get tv show data

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import styledComponents from "styled-components";
+import { contentsApi } from "../../../api";
 import { MainBanner } from "./MainBanner";
 
 const Wrap = styledComponents.div`
@@ -6,6 +8,25 @@ const Wrap = styledComponents.div`
 `;
 
 export const Home = () => {
+  const [onAir, setonAir] = useState();
+
+  useEffect(() => {
+    const contentsData = async () => {
+      try {
+        // ㅂㅏㅇㅇㅕㅇㅈㅜㅇ tvㅅㅛ
+        const {
+          data: { results: tvOnair },
+        } = await contentsApi.tvOnair();
+        setonAir(tvOnair);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    contentsData();
+  }, []);
+  console.log("ㅂㅏㅇㅇㅕㅇㅈㅜㅇ tvㅅㅛ:", onAir);
+
+
   return (
     <Wrap>
       <MainBanner />
