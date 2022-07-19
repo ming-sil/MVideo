6cd73f4b3a1058bc2103b2cd2b0eb5efd1832598
ming-sil <choming4926@gmail.com>
2022-07-20 04:42:14 +0900

    header responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { useForm } from "react-hook-form";
+import { faMagnifyingGlass, faBars } from "@fortawesome/free-solid-svg-icons";
 import { mainStyle } from "../styles/GlobalStyle";
+import { useState } from "react";
 
 const Wrap = styled.header`
   width: 100%;
@@ -16,9 +16,10 @@ const Wrap = styled.header`
   top: 0;
   left: 0;
   z-index: 999;
+  background-color: ${(props) => props.bgColor};
+
   @media screen and (max-width: 500px) {
     padding: ${mainStyle.moPadding};
-    height: 60px;
   }
 `;
 
@@ -34,6 +35,9 @@ const MenuWrap = styled.ul`
     font-weight: 400;
     margin-right: 30px;
   }
+  @media screen and (max-width: 500px) {
+    display: none;
+  }
 `;
 
 const Logo = styled.div`
@@ -48,45 +52,89 @@ const Menu = styled.li``;
 const SearchWrap = styled.form`
   display: flex;
   position: relative;
-  button {
-    all: unset;
-    position: absolute;
-    top: 50%;
-    right: 10px;
-    transform: translateY(-10px);
-    cursor: pointer;
-  }
-  &:hover input {
-    opacity: 1;
+  @media screen and (max-width: 500px) {
+    display: none;
   }
 `;
 
-const Search = styled.input`
-  all: unset;
-  border: 1px solid white;
-  border-radius: 10px;
+const MoMenu = styled.div`
+  display: none;
+  @media screen and (max-width: 500px) {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+`;
+
+const MoMenuWrap = styled.ul`
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+  backdrop-filter: blur(3px);
+  border-radius: 30px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  left: ${(props) => props.leftResult};
   display: flex;
-  padding: 5px 10px;
-  font-size: 13px;
-  opacity: 0;
-  transition: 0.3s;
-  &::placeholder {
-    color: rgba(255, 255, 255, 0.6);
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  transition: 0.5s;
+  li {
+    font-size: 40px;
+    font-weight: 700;
+    margin: 50px;
+    &:nth-child(1) {
+      font-size: 24px;
+      margin: 0;
+    }
+  }
+`;
+
+const MenuBtn = styled.div`
+  font-size: 24px;
+`;
+
+const CloseBtn = styled.li`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  cursor: pointer;
+  z-index: 999;
+  svg {
+    transition: 0.3s;
+    fill: ${mainStyle.mainColor};
   }
 `;
 
 export const Header = () => {
+  const [bg, setBg] = useState("transparent");
+  const [left, setLeft] = useState("100%");
+
+  const handleScroll = () => {
+    const sct = window.pageYOffset;
+    if (300 < sct) {
+      setBg("#1d1d1d");
+    } else {
+      setBg("transparent");
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
   return (
-    <Wrap>
+    <Wrap bgColor={bg}>
       <LogoMenuWrap>
         <Link to="/">
           <Logo>MVideo</Logo>
         </Link>
 
         <MenuWrap>
-          <Link to="/">
-            <Menu>홈</Menu>
-          </Link>
           <Link to="/movies">
             <Menu>영화</Menu>
           </Link>
@@ -96,11 +144,33 @@ export const Header = () => {
         </MenuWrap>
       </LogoMenuWrap>
       <SearchWrap>
-        <Search type="text" placeholder="검색어를 입력하세요"></Search>
-        <button>
+        <Link to="/search">
           <FontAwesomeIcon icon={faMagnifyingGlass} />
-        </button>
+        </Link>
       </SearchWrap>
+
+      <MoMenu>
+        <MenuBtn onClick={() => setLeft(0)}>
+          <FontAwesomeIcon icon={faBars} />
+        </MenuBtn>
+        <MoMenuWrap leftResult={left}>
+          <CloseBtn onClick={() => setLeft("100%")}>
+            <svg viewBox="0 0 512 512">
+              <path d="M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM175 208.1L222.1 255.1L175 303C165.7 312.4 165.7 327.6 175 336.1C184.4 346.3 199.6 346.3 208.1 336.1L255.1 289.9L303 336.1C312.4 346.3 327.6 346.3 336.1 336.1C346.3 327.6 346.3 312.4 336.1 303L289.9 255.1L336.1 208.1C346.3 199.6 346.3 184.4 336.1 175C327.6 165.7 312.4 165.7 303 175L255.1 222.1L208.1 175C199.6 165.7 184.4 165.7 175 175C165.7 184.4 165.7 199.6 175 208.1V208.1z" />
+            </svg>
+          </CloseBtn>
+          <li>
+            <Link to={"/movies"}>🎬영화</Link>
+          </li>
+          <li>
+            <Link to={"/tvprograms"}>📺TV프로그램</Link>
+          </li>
+
+          <li>
+            <Link to={"/search"}>🔍Search</Link>
+          </li>
+        </MoMenuWrap>
+      </MoMenu>
     </Wrap>
   );
 };
